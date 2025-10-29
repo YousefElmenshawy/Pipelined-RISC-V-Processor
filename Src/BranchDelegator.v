55bbe33de5f906c58b3f6519873b3d90689831dd
Kareem Rashed <159137123+Kareem-Rashed@users.noreply.github.com>
2025-10-29 21:46:08 +1300

    Added Branch Delegator
    - to handle different branches

---
+++ Src/BranchDelegator.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/28/2025 05:48:59 PM
+// Design Name: 
+// Module Name: BranchDelegator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BranchDelegator(input ZFlag, CFlag, SFlag, VFlag, Branch, input [2:0] func3, output MuxIn  );
+
+always @(*) begin
+    case (func3)
+      
+        3'b000: begin //BEQ
+        if(Branch && ZFlag) MuxIn = 1'b1;
+        end
+        
+        3'b001: begin  //BNE
+        if(Branch && ~ZFlag) MuxIn = 1'b1;
+        end
+        
+        3'b100: begin  //BLT
+        if(Branch && (SFlag != VFlag)) MuxIn = 1'b1;
+        end
+        
+        3'b101: begin  //BGE
+        if(Branch && (SFlag == VFlag)) MuxIn = 1'b1;
+        end
+        
+        3'b110: begin  //BLTU
+        if(Branch && ~CFlag) MuxIn = 1'b1;
+        end
+        
+        3'b111: begin  //BGEU
+        if(Branch && CFlag) MuxIn = 1'b1;
+        end
+        
+        
+        default: MuxIn = 1'b0;
+
+endcase
+end
+endmodule
