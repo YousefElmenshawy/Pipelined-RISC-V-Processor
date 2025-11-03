98f22c4479834d41b39670261955db9bd1093057
JOEElmenshawy <159122991+JOEElmenshawy@users.noreply.github.com>
2025-11-03 15:51:21 +0200

    Create Program_tb.v
    Added Test Bench to test Different testcases in Instruction Memory

---
+++ TB/Program_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/03/2025 03:42:08 PM
+// Design Name: 
+// Module Name: TC_01
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Program_tb();
+reg clk, rst;
+SingleCycle DUT (clk,rst);
+initial begin
+clk =0 ;
+forever#(5) clk = ~clk;
+
+end
+initial begin
+rst = 0;
+#10
+rst =1;
+
+end
+endmodule
